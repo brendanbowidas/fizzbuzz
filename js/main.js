1eb427385a62627d1ad607151bb3ac277dc81477
brendanbowidas <brendanbowidas@gmail.com>
2015-07-04 16:40:53 -0400

    refactored

--- js/main.js
+++ js/main.js
@@ -1,17 +1,27 @@
-$(document).ready(function(){
-    for(var i = 1; i <= 100; i++) {
-        if(i % 3 === 0 && i % 5 === 0) {
-            $('ul').append('<li>Fizz Buzz</li>');
+$(document).ready(function () {
+
+    var n = parseInt(prompt("Enter a number"), 10);
+    var fizzBuzz = function (n) {
+        if (n != NaN && n % 1 === 0) {
+
+            for (var i = 1; i <= n; i++) {
+                if (i % 3 === 0 && i % 5 === 0) {
+                    $('ul').append('<li>Fizz Buzz</li>');
+                } else if (i % 3 === 0) {
+                    $('ul').append('<li>Fizz</li>');
+                } else if (i % 5 === 0) {
+                    $('ul').append('<li>Buzz</li>');
+                } else {
+                    $('ul').append('<li>' + i + '</li>');
+                }
+            }
+
+        } else {
+            window.location.reload(false);
         }
-        else if(i % 3 === 0) {
-            $('ul').append('<li>Fizz</li>');
-        }
-        else if(i % 5 === 0) {
-            $('ul').append('<li>Buzz</li>');
-        }
-        else {
-            $('ul').append('<li>' + i + '</li>');
-        }
+
+
     };
+    fizzBuzz(n);
 
 });
